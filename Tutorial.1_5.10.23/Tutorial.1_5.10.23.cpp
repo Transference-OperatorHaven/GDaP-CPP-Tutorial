d93bea209384edc6b2583138f507e72336d9bf4f
Haven <leoshelleyyellow8@gmail.com>
2023-10-05 13:38:31 +0100

    Adding project files to the repo

---
+++ Tutorial.1_5.10.23/Tutorial.1_5.10.23.cpp
@@ -0,0 +1,31 @@
+// Tutorial.1_5.10.23.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "-------------\n";
+    cout << "Name: Haven\n";
+    cout << "Course: GD&P\n";
+    cout << "ID: s019465n\n";
+    cout << "-------------\n";
+
+    cout << "  *\n";
+    cout << "  * *\n";
+    cout << "  *  *\n";
+    cout << "  *****\n";
+    cout << "    |  \n";
+    cout << "    |  \n";
+    cout << "*********\n";
+    cout << " *     * \n";
+    cout << "  *****  \n";
+
+    cout << "legally not a boat! \n";
+
+    cout << "hello \t\t\t\t\t there\n";
+    cout << "2 + 2" << "=" << "4\n";
+
+}
